3cbc0b180252b06bae26cf3fc342c8d89917593b
Bobbie <>
2014-11-04 10:43:24 +0100

    Created 2SemProj

---
+++ src/pkg2semproj/Main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pkg2semproj;
+
+/**
+ *
+ * @author Computer
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Umair!");
+        for (String string : args) {
+            System.out.println(string);
+        }
+    }
+    
+}
